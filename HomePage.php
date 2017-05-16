ffbdc0bd439e8ebdc3a1c04fec5a707172e8e487
root <root@Meseguer>
2017-05-16 11:35:17 +0200

    FAv

--- HomePage.php
+++ HomePage.php
@@ -1,5 +1,23 @@
 <!DOCTYPE html>
 <html>
+    <link rel="apple-touch-icon" sizes="57x57" href="Imagenes/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="Imagenes/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="Imagenes/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="Imagenes/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="Imagenes/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="Imagenes/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="Imagenes/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="Imagenes/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="Imagenes/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="Imagenes/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="Imagenes/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="Imagenes/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="Imagenes/favicon-16x16.png">
+<link rel="manifest" href="Imagenes/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
     <head>
         <title>MusicAndSeek</title>
         <meta charset="UTF-8">
@@ -11,7 +29,7 @@
             });
         </script> 
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="Estilos/Estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="ImagenesEstilos/Estilos.css" rel="stylesheet" type="text/css"/>
     </head>
     <body class="center">
         <div id="header"></div></br> </br>  
