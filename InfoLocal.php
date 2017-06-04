9fadd0cc973fbf28e9c28dbd704f431a1fc0407c
root <root@Meseguer>
2017-06-04 13:53:28 +0200

    sdfsfsf

--- InfoLocal.php
+++ InfoLocal.php
@@ -28,22 +28,110 @@
             }
         </script>
         <style>
-            table.one {									 
-                margin-bottom: 3em;	
-                border-collapse:collapse;
-            }	
-            td {							
-                text-align: center;     
-                width: 10em;					
-                padding: 1em; 	
+            .panel-shadow {
+                box-shadow: rgba(0, 0, 0, 0.3) 7px 7px 7px;
             }
-            tr {   
-                height: 2em;    }
-            table tr:nth-child(even) {           
-                background-color: #fff;    
+            .panel-white {
+                border: 1px solid #dddddd;
             }
-            table tr:nth-child(odd) {        
-                background-color:#d83c3c;      
+            .panel-white  .panel-heading {
+                color: #333;
+                background-color: #fff;
+                border-color: #ddd;
+            }
+            .panel-white  .panel-footer {
+                background-color: #fff;
+                border-color: #ddd;
+            }
+
+            .post .post-heading {
+                height: 30px;
+                padding: 20px 15px;
+            }
+            .post .post-heading .avatar {
+                width: 60px;
+                height: 60px;
+                display: block;
+                margin-right: 15px;
+            }
+            .post .post-heading .meta .title {
+                margin-bottom: 0;
+            }
+            .post .post-heading .meta .title a {
+                color: black;
+            }
+            .post .post-heading .meta .title a:hover {
+                color: #aaaaaa;
+            }
+            .post .post-heading .meta .time {
+                margin-top: 8px;
+                color: #999;
+            }
+            .post .post-image .image {
+                width: 100%;
+                height: auto;
+            }
+            .post .post-description {
+                padding: 15px;
+                max-width: 300px;
+            }
+            .post .post-description p {
+                font-size: 14px;
+            }
+            .post .post-description .stats {
+                margin-top: 20px;
+            }
+            .post .post-description .stats .stat-item {
+                display: inline-block;
+                margin-right: 15px;
+            }
+            .post .post-description .stats .stat-item .icon {
+                margin-right: 8px;
+            }
+            .post .post-footer {
+                border-top: 1px solid #ddd;
+                padding: 15px;
+            }
+            .post .post-footer .input-group-addon a {
+                color: #454545;
+            }
+            .post .post-footer .comments-list {
+                padding: 0;
+                margin-top: 20px;
+                list-style-type: none;
+            }
+            .post .post-footer .comments-list .comment {
+                display: block;
+                width: 100%;
+                margin: 20px 0;
+            }
+            .post .post-footer .comments-list .comment .avatar {
+                width: 35px;
+                height: 35px;
+            }
+            .post .post-footer .comments-list .comment .comment-heading {
+                display: block;
+                width: 100%;
+            }
+            .post .post-footer .comments-list .comment .comment-heading .user {
+                font-size: 14px;
+                font-weight: bold;
+                display: inline;
+                margin-top: 0;
+                margin-right: 10px;
+            }
+            .post .post-footer .comments-list .comment .comment-heading .time {
+                font-size: 12px;
+                color: #aaa;
+                margin-top: 0;
+                display: inline;
+            }
+            .post .post-footer .comments-list .comment .comment-body {
+                margin-left: 50px;
+            }
+            .post .post-footer .comments-list .comment > .comments-list {
+                margin-left: 50px;
+
             }
         </style>
         <link href="Estilos/Estilos.css" rel="stylesheet" type="text/css"/>
@@ -94,15 +182,29 @@
             echo '<div class="center">Descripción : ' . $resultado['DESCRIPCION'] . '</div>';
         }
         if ($comentarios != false) {
-            echo '</br></br><div class="center">Comentarios : <table class="center">';
+            echo '</br></br><div class="container">';
             while ($lista = $comentarios->fetch_assoc()) {
-                echo'<tr><th>';
-                $fechacomentario = getNombreFecha(date("w-d-m-Y", strtotime($lista['FECHA'])));
-                echo $lista['NOMBRE'] . ' - ' . $fechacomentario . '</th></tr>';
-
-                echo '<tr><th>' . $lista['COMENTARIO'] . '</th></tr>';
+                echo'<div class="row">
+                    <div class="col-sm-8">
+                    <div class="panel panel-white post panel-shadow">
+                    <div class="post-heading">
+                    <div class="pull-left meta">
+                    <div class="title h5">
+                    <a href="#"><b>' . $lista['NOMBRE'] . ' ' . $lista['APELLIDOS'] . '</b></a>
+                            ha realizado un comentario
+                        </div>
+                        <h6 class="text-muted time">El ' . getNombreFecha(date("w-d-m-Y", strtotime($lista['FECHA']))) . '</h6>
+                    </div>
+                </div> 
+                <div class="post-description"> 
+                   <p> ' . $lista['COMENTARIO'] . '</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>';
             }
-            echo'</table></div>';
+            echo'</div>';
         }
         echo '</div></div>';
 
