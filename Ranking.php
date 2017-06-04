fa813d6108300a314387dca02d5c3acfca328acd
root <root@Meseguer>
2017-06-04 12:51:54 +0200

    Merge origin/master
    Conflicts:
    	bbdd.php

--- Ranking.php
+++ Ranking.php
@@ -15,17 +15,20 @@
         <link href="Estilos/StarRating.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div id="header"></div>   
+        <div id="header"></div> 
+        <h1 class="center"><span class="color_rojo_general">Top</span> 5 Mundial </h1>
         <div id="content center">
+            <div style="height: 50px"></div>
             <div class="inline center"  style="display:inline;">
                 <?php
                 require_once 'bbdd.php';
                 echo '<div style="width:100%">';
                 echo '<div class="inline" style="width:50%">';
+                echo'<h1><span class="color_rojo_general">Artistas</span></h1>';
                 ArtistasAlza(null, null, '');
                 echo '</div>';
                 echo '<div class="inline" style="width:50%">';
-                echo'<h1><span class="color_rojo_general">Locales</span> en Alza</h1>';
+                echo'<h1><span class="color_rojo_general">Locales</span></h1>';
                 LocalesAlza(null, '');
                 echo '</div>';
                 echo '</div>';
