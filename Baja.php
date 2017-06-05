3342c0fd1bd0554924c2a7d08698f050ecdc7dab
root <root@Meseguer>
2017-06-05 21:06:49 +0200

    Baja

--- Baja.php
+++ Baja.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="Estilos/Estilos.css" rel="stylesheet" type="text/css"/>
+        <link href="Estilos/RegistrationForm.css" rel="stylesheet" type="text/css"/>
         <?php
         session_start();
         require_once'bbdd.php';
-        if (isset($_SESSION['pass'])) {
-
-            $result = darBaja($_SESSION['email']);
-            if ($result) {
-                echo'           
+        if (isset($_SESSION['pass']) && !isset($_POST['no']) && !isset($_POST['si'])) {
+            echo'           
         <title>Baja Usuario</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,18 +18,50 @@
                 $("#footer").load("Footer.html");
             });
         </script> 
-        <link href="../Estilos/Estilos.css" rel="stylesheet" type="text/css"/>
+
     </head>
     <body>
         <div id="header"></div> 
         <div class="content center">
-            <h1><span class="color_rojo_general">Usuario </span>dado de <span class="color_rojo_general">Baja </span>Correctamente</h1>
+            <h1>¿ Seguro que quieres dar de <span class="color_rojo_general">Baja  </span>el <span class="color_rojo_general">Usuario ? </span></h1>
+            <form  class="center" style="width:1000px" action="" method="POST" id="msform">        
+            <div>
+                <div class="inline">
+                    <button style="width:400px" type="submit" class="submit action-button"  name = "no">No</button>
+                </div>
+                <div class="inline">
+                    <button style="width:400px" type="submit" class="submit action-button"  name = "si">Si</button>
+                </div>
+            </div>
+            </form>
         </div>
         <div id="footer"></div>
     </body>
 </html>';
-                header("refresh:2; url=Logout.php");
-            }
+        } elseif (isset($_POST['no'])) {
+            header("refresh:0; url=Perfil.php");
+        } elseif (isset($_POST['si'])) {
+            $result = darBaja($_SESSION['email']);
+            echo '   <title>Baja Usuario</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script>
+            $(function () {
+                $("#header").load("Header.php");
+                $("#footer").load("Footer.html");
+            });
+        </script> 
+    </head>
+    <body>
+        <div id="header"></div> 
+        <div class="content center">
+            <h1><span class="color_rojo_general">Usuario </span>dado de baja <span class="color_rojo_general">Correctamente</span></h1>
+        </div>
+        <div id="footer"></div>
+    </body>
+</html>';
+            header("refresh:2; url=Logout.php");
         } else {
             header("refresh:0; url=HomePage.php");
         }
